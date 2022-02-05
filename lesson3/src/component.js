6bcf3d77f68cadd3b4b8c2bcdb5f9c324513ea89
Hadi Ahmed <hadiahmed098@gmail.com>
2022-02-05 11:24:15 -0500

    Added a blank component file

---
+++ lesson3/src/component.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class /*FILENAME*/ extends React.Component {
+
+    // Ignore this for now...
+    constructor(props) {
+        super(props)
+    }
+
+    render () {
+        // Type your code here...
+
+
+
+        // Return some JSX here...
+        return null;
+    }
+}
+
+
+export default /*FILENAME*/;
