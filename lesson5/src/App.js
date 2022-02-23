8114475b2708886866b0d0f7305ab0ca4a37a3b8
Sam Faber <samfaber@Sams-MacBook-Air.local>
2022-02-22 20:52:27 -0500

    Added lesson 5 checkpoint 1 example code

--- lesson5/src/App.js
+++ lesson5/src/App.js
@@ -8,20 +8,30 @@ function App() {
   // For checkpoint 1: Write code here similar to Demo2.js
   // Hint: Create an array and map each element to JSX (the Card component), then add it to the JSX you're returning
 
+  // Checkpoint 1 Example code:
+  const names = ["bulbasaur", "charmander", "squirtle"];
+  const namesJSX = names.map((pokeName) => <Card name={pokeName} />);
+
+
   return (
     // Type your website layout here...
-    <div id="demos">
-      {/* Demo #1 */}
-      <Demo1 />
-      <hr />
+    <div>
+      {/* Checkpoint 1 Example code: */}
+      {namesJSX}
 
-      {/* Demo #2 */}
-      {/* <Demo2 numbers={[1,2,3,4,5]}/>
-      <hr /> */}
+      <div id="demos">
+        {/* Demo #1 */}
+        <Demo1 />
+        <hr />
 
-      {/* Demo #3 */}
-      {/* <Demo3 /> */}
+        {/* Demo #2 */}
+        {/* <Demo2 numbers={[1,2,3,4,5]}/>
+        <hr /> */}
 
+        {/* Demo #3 */}
+        {/* <Demo3 /> */}
+
+      </div>
     </div>
   );
 }
